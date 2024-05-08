f92735a3625ba6d255feca767ec0b6c8cd73cc8d
Raido <admin@example.com>
2024-05-08 22:22:50 +0300

    First things first

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css"> 
+   
+</head>
+<body>
+    <article>
+        <h1><a href="/post"> My first post</a></h1>
+        <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, illo delectus reprehenderit corrupti qui rem? Minima rem ipsum nihil aliquam dicta mollitia, consequatur iste vel dolore illum quam modi laboriosam nesciunt eos quaerat et recusandae fuga deleniti reprehenderit sequi odit ea. Recusandae doloremque accusantium, voluptas quam ullam incidunt qui sint! Nesciunt dicta labore natus porro deleniti, enim aspernatur officia eligendi rem sit, at, reprehenderit asperiores? Dicta exercitationem et nobis explicabo iusto aliquam dolor, soluta maiores natus, enim quidem cupiditate autem laborum quo sint at sunt, hic magnam! Nam necessitatibus iure
+    </article>
+    <article>
+        <h1><a href="/post">My second post</a></h1>
+        <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, illo delectus reprehenderit corrupti qui rem? Minima rem ipsum nihil aliquam dicta mollitia, consequatur iste vel dolore illum quam modi laboriosam nesciunt eos quaerat et recusandae fuga deleniti reprehenderit sequi odit ea. Recusandae doloremque accusantium, voluptas quam ullam incidunt qui sint! Nesciunt dicta labore natus porro deleniti, enim aspernatur officia eligendi rem sit, at, reprehenderit asperiores? Dicta exercitationem et nobis explicabo iusto aliquam dolor, soluta maiores natus, enim quidem cupiditate autem laborum quo sint at sunt, hic magnam! Nam necessitatibus iure 
+    </article>
+    <article>
+        <h1><a href="/post">My third post</a></h1>
+        <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, illo delectus reprehenderit corrupti qui rem? Minima rem ipsum nihil aliquam dicta mollitia, consequatur iste vel dolore illum quam modi laboriosam nesciunt eos quaerat et recusandae fuga deleniti reprehenderit sequi odit ea. Recusandae doloremque accusantium, voluptas quam ullam incidunt qui sint! Nesciunt dicta labore natus porro deleniti, enim aspernatur officia eligendi rem sit, at, reprehenderit asperiores? Dicta exercitationem et nobis explicabo iusto aliquam dolor, soluta maiores natus, enim quidem cupiditate autem laborum quo sint at sunt, hic magnam! Nam necessitatibus iure 
+    </article>
+</body>
+</html>
