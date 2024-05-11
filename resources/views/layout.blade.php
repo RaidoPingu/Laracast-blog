27d8aeeb3b4171e529d3ead54c744048dd0e198a
Raido <admin@example.com>
2024-05-11 21:04:24 +0300

    Vahepealne

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,1 +1,35 @@
-<?php
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+<body>
+@unless(true)
+
+@endunless
+
+
+
+@foreach ($posts as $post)
+
+    <article class="{{ $loop->even ? 'foobar' : '' }}">
+        <h1>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
+            </a>
+
+        </h1>
+
+
+        <div>
+            {{ $post->excerpt }}
+        </div>
+
+    </article>
+@endforeach
+</body>
+</html>
