0d9c077bb7c3aa75eb1f7e9820c8c2ee9a7377d7
Raido <admin@example.com>
2024-05-14 23:49:31 +0300

    I fustrates me to no end! Done for the day.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,34 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
-    @foreach ($posts as $post)
+<x-layout>
+    @include('_posts-header')
 
-        <article>
-            <h1>
-                <a href="/posts/{{ $post-> slug }}">
-                    {!! $post->title !!}
-                </a>
-
-            </h1>
-
-            <p>
-                By <a href="/authors/{{$post->author->id}}">{{$post->author->username}}</a> in  <a href="/categories/{{$post->category->slug}}">{{ $post->category->name }}</a>
-            </p>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-posts-grid :posts="$posts"/>
 
 
-            <div>
-                {{ $post->excerpt }}
-            </div>
-
-        </article>
-        @endforeach
-
-</body>
-</html>
+    </main>
+</x-layout>
 
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-      'posts' => Post::latest('published_at')
+      'posts' => Post::latest()->get(),
+      'categories' => Category::all()
    ]);
 });
 
